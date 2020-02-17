aaa3ceb591ac02b5b03f1324bcb99d8276b1e6f6
Jose DelaCruz <jodelac@microsoft.com>
2020-02-17 14:52:52 -0500

    Updates the condition for UI testing for consistency

--- Permisos.UITests/MenuTests.cs
+++ Permisos.UITests/MenuTests.cs
@@ -7,88 +7,113 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Permisos.UITests.Pages;
 
-namespace Permisos.UITests {
-    [TestClass]
-    public class MenuTests {
-        private IWebDriver _driver;
+namespace Permisos.UITests
+{
+  [TestClass]
+  public class MenuTests
+  {
+    private IWebDriver _driver;
 
-        private string _appURL = Constants.APP_URL;
+    private string _appURL = Constants.APP_URL;
 
-        [TestInitialize()]
-        public void BeforeEach() {
-            _driver = _driver.SetupForBrowser("Chrome");
-            _driver.Navigate().GoToUrl(_appURL);
-        }
-
-        [TestCleanup()]
-        public void AfterEach() {
-            _driver.Quit();
-            _appURL = $"{Constants.APP_URL}/ver";
-        }
-
-        [TestMethod, TestCategory("Chrome")]
-        public void PuedeNavegarAVerPermisos() {
-            // arrange
-            var homepage = new SolicitarPermisoWebPage(_driver);
-
-            // act
-            homepage.VerPermisosLink.Click();
-
-            // assert
-            for (var second = 0; ; second++) {
-                if (second >= 4) {
-                    Assert
-                      .Fail("timeout, should not take more than 4 seconds to redirect");
-                }
-
-                try {
-                    if (_driver.FindElements(By.CssSelector(".card")).Any()) {
-                        break;
-                    }
-                } catch (Exception e) {
-                    throw e;
-                }
-
-                Thread.Sleep(1000);
-            }
-        }
-
-        [TestMethod, TestCategory("Chrome")]
-        public void PuedeNavegarASolicitudPermisos() {
-            // arrange
-            var homepage = new SolicitarPermisoWebPage(_driver);
-
-            // act
-            homepage.SolicitarPermisoLink.Click();
-
-            // assert
-            for (var second = 0; ; second++) {
-                if (second >= Constants.MINIMUM_DELAY) {
-                    Assert
-                      .Fail($"timeout, should not take more than {Constants.MINIMUM_DELAY} seconds to redirect");
-                }
-
-                try {
-                    if (
-                      _driver
-                        .FindElements(By
-                          .XPath("//h2[contains(text(),'Solicitud the Permiso')]"))
-                        .Any()
-                    ) {
-                        break;
-                    }
-                } catch (Exception e) {
-                    throw e;
-                }
-
-                Thread.Sleep(1000);
-            }
-        }
-
-        //////
-        private void SetupSeleniumDriver(string browser) {
-            Debug.Write($"//TODO: {browser}");
-            _driver = new ChromeDriver();
-        }
+    [TestInitialize()]
+    public void BeforeEach()
+    {
+      _driver = _driver.SetupForBrowser("Chrome");
+      _driver.Navigate().GoToUrl(_appURL);
     }
+
+    [TestCleanup()]
+    public void AfterEach()
+    {
+      _driver.Quit();
+      _appURL = $"{Constants.APP_URL}/ver";
+    }
+
+    [TestMethod, TestCategory("Chrome")]
+    public void PuedeNavegarAVerPermisos()
+    {
+      // arrange
+      var homepage = new SolicitarPermisoWebPage(_driver);
+
+      // act
+      homepage.VerPermisosLink.Click();
+
+      // assert
+      for (var second = 0; ; second++)
+      {
+        if (second >= 4)
+        {
+          Assert
+            .Fail("timeout, should not take more than 4 seconds to redirect");
+        }
+
+        try
+        {
+          if (
+            _driver
+              .FindElements(By
+                .XPath("//h2[contains(text(),'Listado de permisos')]"))
+              .Any()
+          )
+          {
+            break;
+          }
+        }
+        catch (Exception e)
+        {
+          throw e;
+        }
+
+        Thread.Sleep(1000);
+      }
+    }
+
+    [TestMethod, TestCategory("Chrome")]
+    public void PuedeNavegarASolicitudPermisos()
+    {
+      // arrange
+      var homepage = new SolicitarPermisoWebPage(_driver);
+
+      // act
+      homepage.SolicitarPermisoLink.Click();
+
+      // assert
+      for (var second = 0; ; second++)
+      {
+        if (second >= Constants.MINIMUM_DELAY)
+        {
+          Assert
+            .Fail($"timeout, should not take more than " +
+            "{Constants.MINIMUM_DELAY} seconds to redirect");
+        }
+
+        try
+        {
+          if (
+            _driver
+              .FindElements(By
+                .XPath("//h2[contains(text(),'Solicitud the Permiso')]"))
+              .Any()
+          )
+          {
+            break;
+          }
+        }
+        catch (Exception e)
+        {
+          throw e;
+        }
+
+        Thread.Sleep(1000);
+      }
+    }
+
+    //////
+    private void SetupSeleniumDriver(string browser)
+    {
+      Debug.Write($"//TODO: {browser}");
+      _driver = new ChromeDriver();
+    }
+  }
 }
